e2da804f5a689062f4ba43de56a1efc3f812aa94
kummer83 <62803311+kummer83@users.noreply.github.com>
2020-04-11 12:54:48 +0300

    Update Init.groovy

--- src/main/java/Setup/Init.groovy
+++ src/main/java/Setup/Init.groovy
@@ -10,8 +10,8 @@ class Init extends Specification {
     public static RESTClient client = new RESTClient(TEST_URL)
 
     // Admin credentials (after updating default password) - used to authenticate.
-    public static String USERNAME = "admin"
-    public static String PASSWORD = "password"
+    public static String USERNAME = "kummer"
+    public static String PASSWORD = "kummer120"
 
     def setupSpec() {
 
